a78df835a958f2264062235a016fbd192fee6fa7
pqtruong0112 <quangtruong01122003@gmail.com>
2025-07-08 20:51:30 +0700

    Update model files, exclude appsettings.Development.json

--- ProjectPRN222/Models/Notification.cs
+++ ProjectPRN222/Models/Notification.cs
@@ -1,28 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 
 namespace ProjectPRN222.Models;
 
 public partial class Notification
 {
-    [Display(Name = "Mã thông báo")]
     public int NotificationId { get; set; }
 
-    [Display(Name = "Mã người dùng")]
-    [Required(ErrorMessage = "Vui lòng chọn người dùng")]
     public int UserId { get; set; }
 
-    [Display(Name = "Nội dung thông báo")]
-    [Required(ErrorMessage = "Vui lòng nhập nội dung thông báo")]
-    [StringLength(1000, ErrorMessage = "Nội dung thông báo không được vượt quá 1000 ký tự")]
     public string Message { get; set; } = null!;
 
-    [Display(Name = "Ngày gửi")]
-    [DataType(DataType.DateTime)]
     public DateTime? SentDate { get; set; }
 
-    [Display(Name = "Đã đọc")]
     public bool? IsRead { get; set; }
 
     public virtual User User { get; set; } = null!;
