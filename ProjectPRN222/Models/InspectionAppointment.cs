333af18be63e187cffcd1b40fe234dda3cbdaec8
pqtruong0112 <quangtruong01122003@gmail.com>
2025-07-08 15:46:54 +0700

    new update appointment

---
+++ ProjectPRN222/Models/InspectionAppointment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectPRN222.Models;
+
+public partial class InspectionAppointment
+{
+    public int AppointmentId { get; set; }
+
+    public int VehicleId { get; set; }
+
+    public int UserId { get; set; }
+
+    public int StationId { get; set; }
+
+    public DateTime AppointmentDate { get; set; }
+
+    public string Status { get; set; } = null!;
+
+    public string? Note { get; set; }
+
+    public virtual InspectionStation Station { get; set; } = null!;
+
+    public virtual User User { get; set; } = null!;
+
+    public virtual Vehicle Vehicle { get; set; } = null!;
+}
